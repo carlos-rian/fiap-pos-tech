d694bfd7625391e17e2019d275973dcde694e53c
carlos-rian <crian.rian@gmail.com>
2025-05-09 16:07:10 -0700

    Add logger configuration for video_analysis module

---
+++ tech-challenge-04/video_analysis/logger.py
@@ -0,0 +1,12 @@
+"""
+Logger configuration for the video_analysis module.
+
+This module sets up and configures a dedicated logger for use throughout the project.
+It specifies the logging format and debug level for debugging and informational purposes.
+"""
+
+import logging
+
+Logger = logging.getLogger("video_analysis")
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+Logger.setLevel(logging.DEBUG)
