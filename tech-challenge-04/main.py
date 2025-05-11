84300286381b2cc37b84367508b086c8b3d10a96
carlos-rian <crian.rian@gmail.com>
2025-05-11 15:51:33 -0700

    Update main function to set window_size parameter for activity detection

--- tech-challenge-04/main.py
+++ tech-challenge-04/main.py
@@ -162,6 +162,7 @@ def main():
         output_analysis_json=output_activity_detector_json,
         window_size=32,
     )
+
     # audio summarization
     audio_summarization(input_video, output_folder)
 
