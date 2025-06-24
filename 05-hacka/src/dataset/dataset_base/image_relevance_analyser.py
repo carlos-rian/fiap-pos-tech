0de76e0c2d50d2e23445134ead3cb42252f45635
carlos-rian <carlos.rian@quartile.com>
2025-06-24 11:13:27 -0300

    add the output  dataset

--- 05-hacka/src/dataset/dataset_base/image_relevance_analyser.py
+++ 05-hacka/src/dataset/dataset_base/image_relevance_analyser.py
@@ -1,5 +1,14 @@
+from pathlib import Path
+
 import pandas as pd
 
-df = pd.read_csv("dataset_base/mapping_images_with_relevance.csv")
+base_path = Path(__file__).parent
+filepath = base_path / "mapping_images_with_relevance.csv"
+df = pd.read_csv(filepath)
 relevance_counts = df["RelevanceName"].value_counts()
-relevance_counts.to_csv("relevance_levels_count.csv", header=True, index=True)
+relevance_counts.to_csv(base_path / "relevance_levels_count.csv", header=True, index=True)
+
+# relevance by prefix resource and relevance name
+df["PrefixResource"] = df["ImageName"].str.split("_").str[0]
+relevance_by_prefix = df.groupby(["PrefixResource", "RelevanceName"]).size().reset_index(name="Count")
+relevance_by_prefix.to_csv(base_path / "relevance_by_prefix.csv", header=True, index=False)
